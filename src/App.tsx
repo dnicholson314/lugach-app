23a63be9dfd955dc7710cbfc9c08f51c9c11863b
Dennis Nicholson <143367057+dnicholson314@users.noreply.github.com>
2026-02-04 01:08:44 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import Button from "@mui/material/Button";
+
+const root = createRoot(document.body);
+root.render(<Button variant="contained">Hello</Button>);
